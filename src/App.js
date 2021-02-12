a4a6d95912cbf1671b5ed5fef15a6bbe312d5692
Ruhan Khandakar <khandakarruhan@gmail.com>
2021-02-12 14:22:49 +0530

    feat: rendering data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { Button } from "antd";
+import { Row, Col, Card } from "antd";
+
+// Custom Components
+import Data from "./components/Data";
 
 const App = () => {
   return (
     <div>
-      <Button type="primary">Button</Button>
+      <Row>
+        <Col span={12} offset={6}>
+          <Card title={null} bordered={false}>
+            <Data />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
---
+++ src/components/Data.jsx
@@ -0,0 +1,67 @@
+import { Table } from "antd";
+import { CheckCircleTwoTone, CloseCircleTwoTone } from "@ant-design/icons";
+
+// Others
+import dataSource from "../data/data.json";
+
+const Data = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Screen Name",
+      dataIndex: "screen_name",
+      key: "screen_name",
+    },
+    {
+      title: "Followers Count",
+      dataIndex: "followers_count",
+      key: "followers_count",
+    },
+    {
+      title: "Friends Count",
+      dataIndex: "friends_count",
+      key: "friends_count",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      key: "location",
+    },
+    {
+      title: "Verified",
+      dataIndex: "verified",
+      key: "verified",
+      render: (data) =>
+        data ? (
+          <CheckCircleTwoTone
+            twoToneColor="#52c41a"
+            style={{
+              fontSize: "20px",
+            }}
+          />
+        ) : (
+          <CloseCircleTwoTone
+            twoToneColor="#eb2f96"
+            style={{
+              fontSize: "20px",
+            }}
+          />
+        ),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+      pagination={{ pageSize: 100, hideOnSinglePage: true }}
+      scroll={{ y: 300 }}
+    />
+  );
+};
+
+export default Data;
